393da49643d807fb54e896328bbee29a35656338
unknown <takkychancnt@gmail.com>
2020-08-29 21:54:00 +0800

    Website Built 29/28/20 -Signed by Takky Chan

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -13,6 +13,11 @@ export default function Footer() {
         <>
             <section id="footer" className="footer">
                 <div className="footer-container">
+                    <div className="media-cta">
+                        <a href="https://github.com/takkychan/profile" target="_blank" className="github"> <i class="fab fa-github"></i> <div className="media-name">Github</div></a>
+                        <a href="https://mail.google.com/mail/?view=cm&source=mailto&to=takkychancnt@gmail.com"  target="_blank" className="email"> <i class="fas fa-envelope"></i> <div className="media-name"><a>takkychanant@gmail.com</a></div></a>
+                        <a className="phone"> <i class="fas fa-mobile-alt"></i><div>69088999</div></a>
+                    </div>
                     <div className="copyright">
                         Copyright © TCDESIGN @ Chan Hok Wan Takky 2020 - A Full stack web developer in Hong Kong 
                     </div>
--- src/home/Header.jsx
+++ src/home/Header.jsx
@@ -62,7 +62,7 @@ export default function Header(props) {
         },
         {
             id: 'contact',
-            link: 'mailto:takkychancnt@gmail.com',
+            link: '#footer',
             name: 'Contact Me',
             isActive: true
         }
@@ -79,17 +79,13 @@ export default function Header(props) {
                         <div></div>
                         </div>
                 </div>
-                {/* {
-                    openMenu ? 
-                    <div className="burger-header">
-                        <Nav navItems = {navItems}/> 
-                    
-                    </div>
-                    :
-                    null
-                } */}
                     <div className="burger-header" style={openMenu ? {transform: "translate(0, 0)"} : {transform: "translate(100%, 0)"}}>
                         <Nav navItems = {navItems}/> 
+                        <div className="media-cta">
+                        <a href="https://github.com/takkychan/profile" target="_blank" className="github"> <i class="fab fa-github"></i> <div className="media-name">Github</div></a>
+                        <a href="https://mail.google.com/mail/?view=cm&source=mailto&to=takkychancnt@gmail.com"  target="_blank" className="email"> <i class="fas fa-envelope"></i> <div className="media-name"><a>takkychanant@gmail.com</a></div></a>
+                        <a className="phone"> <i class="fas fa-mobile-alt"></i><div>69088999</div></a>
+                    </div>
                     </div>
                 <div className="header" ref={theHeader}>
              
